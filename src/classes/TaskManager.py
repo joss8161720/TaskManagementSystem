7814793917ce43769e13cd5c692872e5682fc2e4
Joss <122181428+CodaQuell@users.noreply.github.com>
2025-05-18 21:18:54 +1000

    main and TaskManager update

--- src/classes/TaskManager.py
+++ src/classes/TaskManager.py
@@ -32,19 +32,35 @@ class TaskManager:
         due_date_input = input("Set Due Date (e.g 01/01/2025) >> ")
         category_input = input("Set Category (e.g Home, Work) >> ")
         priority_input = input("Set Priority Level (High/Medium/Low) >> ")
-        dependencies_input = input("Set Dependencies (Tasks to be completed before this task, press enter for no dependencies) >> ")
+        dependencies_input = input("Set Dependencies (Tasks to be completed before this task comma seperated, press enter for no dependencies) >> ")
         print(" ")
+        
 
         #need to keep adding to dependencies to make them usefull
         #splits a sting into a list then remakes it as a string, if it equals "" return "" as value
-        dependencies_list = ", ".join(dependencies_input.split()) if dependencies_input else ""
+        dependencies = ", ".join(dependencies_input.split()) if dependencies_input else ""
+
+        if dependencies != "":
+            dependencies_list = dependencies.split(", ")
+
+            for used_dep in dependencies_list:
+                found = False
+                for task in self.tasks:
+                    if used_dep == task.name:
+                        found = True
+                        break
+
+                if found:
+                    print(f"Dependency '{used_dep}' found.")
+                else:
+                    print(f"Warning: Dependency '{used_dep}' not found in existing tasks.")
 
         task = Task(
             name_input,
             due_date_input,
             category_input,
             priority_input,
-            dependencies_list
+            dependencies
         )
 
         #adds task object to list of tasks
@@ -54,7 +70,7 @@ class TaskManager:
         print(f"{name_input} name: {task.name}\n{name_input} due date: {task.due_date}\n{name_input} category: {task.category}\n{name_input} priority: {task.priority}")
 
         #checks if printing dependencies is necessary
-        if dependencies_list != "":
+        if dependencies != "":
             print(f"{name_input} dependencies: {task.dependencies}")
         else:
             pass
@@ -64,8 +80,10 @@ class TaskManager:
         #checks if there if tasks has any entries
         if not self.tasks:
             print("No tasks to display")
+
         else:
             print("Your tasks:")
+            
             #loops over list tasks and keeps track of the index of each item
             for i, task in enumerate(self.tasks, 1):
                 print(f"Task {i}:{task}")
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ task_manager = TaskManager()
 #while loop for user to enter commands
 while True:
 
-    print(f"\nOptions:\n1: Add task\n2: View Tasks\n3: Mark Completed\n4: Quit")
+    print(f"\nOptions:\n1: Add task\n2: View Tasks\n3: Mark Completed\n4: Sort\n5: Quit")
     user_input = input(">> ")
     
     #takes user input and calls function from TaskManager according to the command entered
@@ -18,3 +18,8 @@ while True:
         task_manager.view_tasks()
     elif user_input == "3":
         task_manager.mark_task_complete()
+    elif user_input == "5":
+        print("Exiting program")
+        break
+    elif user_input == "4":
+        pass
